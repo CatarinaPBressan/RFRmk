8212773107bdb9b0c7b66aab2bb6f11951754ace
HardDiskD <arthurbressan2@hotmail.com>
2013-05-09 21:45:33 -0300

    created basic health behaviour

---
+++ Assets/HealthCounter.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+
+public class HealthCounter : MonoBehaviour {
+
+	// Use this for initialization
+
+    public int InitialHealth = 100;
+
+
+    private int CurrentHealth;
+	void Start () {
+        CurrentHealth = InitialHealth;
+	}
+
+    public int GetCurrentHealth()
+    {
+        return CurrentHealth;
+    }
+
+    public void Restore(int qtyToRestore)
+    {
+        if (qtyToRestore < 0)
+        {
+            return;
+        }
+        ChangeCurrentHealth(qtyToRestore);
+    }
+    
+    public void Damage(int qtyToDamage)
+    {
+        if (qtyToDamage < 0)
+        {
+            return;
+        }
+        ChangeCurrentHealth(-qtyToDamage);
+    }
+
+    private void ChangeCurrentHealth(int quantity)
+    {
+        CurrentHealth += quantity;
+
+        if (CurrentHealth > InitialHealth)
+        {
+            CurrentHealth = InitialHealth;
+        }
+
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            Destroy(this.gameObject); //temp
+        }
+    }
+}
