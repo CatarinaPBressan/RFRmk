7515c5b6288aea8ba26f830189c15700c54c258e
HardDiskD <arthurbressan2@hotmail.com>
2013-05-09 21:25:39 -0300

    basic health behaviour done

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -4,23 +4,30 @@ using System;
 
 public class Projectile : MonoBehaviour {
 
-    public float speed = 1;
-    public float lifeMs = 1000;
-
-
-    private DateTime startTime;
+    public float Speed = 1;
+    public float LifeMs = 1000;
+    public int Damage = 10;
 	// Use this for initialization
 	void Start () {
-        Destroy(this.gameObject, lifeMs / 1000); //schedule the object for deletion
+        Destroy(this.gameObject, LifeMs / 1000); //schedule the object for deletion
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        this.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
 	}
 
     void OnCollisionEnter(Collision collisionInfo)
     {
+        HealthCounter hc = collisionInfo.gameObject.GetComponent<HealthCounter>();
+
+        if (hc != null)
+        {
+            hc.Damage(this.Damage);
+            Debug.Log("damaged!");
+        }
+
         Destroy(this.gameObject);
     }
+
 }
