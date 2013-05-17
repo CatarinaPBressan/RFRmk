0c4ca5749def7291c3e9f75238cfcbe2c7675d4e
Arthur Bressan <arthurpbressan@gmail.com>
2013-05-17 14:52:54 -0300

    created .meta files
    also fixed the issue that was creating a flag while the application was
    exiting

--- Assets/Scripts/FlagTower.cs
+++ Assets/Scripts/FlagTower.cs
@@ -7,6 +7,7 @@ public class FlagTower : MonoBehaviour {
     public Team team = Team.Brown;
 
     public GameObject flagGO;
+    private bool isQuitting = false;
 
 
 	// Use this for initialization
@@ -19,15 +20,24 @@ public class FlagTower : MonoBehaviour {
 	
 	}
 
+    void OnApplicationQuit()
+    {
+        isQuitting = true;
+    }
+
+
     void OnDestroy()
     {
-        if (hasFlag)
+        if (!isQuitting)
         {
-            GameObject flag = GameObject.Instantiate(flagGO, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
-            FlagBehaviour fb = flag.GetComponent<FlagBehaviour>();
+            if (hasFlag)
+            {
+                GameObject flag = GameObject.Instantiate(flagGO, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+                FlagBehaviour fb = flag.GetComponent<FlagBehaviour>();
 
-            fb.team = this.team;
-            
+                fb.team = this.team;
+
+            }
         }
     }
 }
