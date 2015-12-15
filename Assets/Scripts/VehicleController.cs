35c8341cb99cda2dc026ef4e4e74212a97ae6e1d
Rômulo Lima <romulolink@gmail.com>
2015-12-15 00:34:31 -0200

    Inclusao de indicador de combustivel

--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class VehicleController : MonoBehaviour {
 
@@ -11,8 +12,8 @@ public class VehicleController : MonoBehaviour {
     public float MovingThreshold = 4f;
     public int MaxFuelUnits = 100;
     public bool ConsumeFuelOnTurn = false;
-
-    private int CurrentFuelUnits;
+    private float CurrentFuelUnits;
+    private Scrollbar FuelIndicator;
     private bool IsMoving;
     private static readonly int FUEL_CONSUMPTION_RATE = 1;
 
@@ -20,10 +21,16 @@ public class VehicleController : MonoBehaviour {
     void Start()
     {
         CurrentFuelUnits = MaxFuelUnits;
+
+        FuelIndicator = GetComponentInChildren<Scrollbar>();
     }
 
     void FixedUpdate()
     {
+        Debug.Log("valor do fuel = "  + (CurrentFuelUnits / 100));
+        Debug.Log("valor do size indicador = " + FuelIndicator.size);
+        FuelIndicator.size =  CurrentFuelUnits / 100f;
+
         IsMoving = CheckMoving();
     }
 
@@ -31,7 +38,7 @@ public class VehicleController : MonoBehaviour {
     {
         if (CurrentFuelUnits > 0)
         {
-            this.rigidbody.AddForce(this.transform.forward * (this.rigidbody.mass * ForwardPower));
+            this.GetComponent<Rigidbody>().AddForce(this.transform.forward * (this.GetComponent<Rigidbody>().mass * ForwardPower));
             CurrentFuelUnits -= FUEL_CONSUMPTION_RATE;
         }
     }
@@ -40,7 +47,7 @@ public class VehicleController : MonoBehaviour {
     {
         if (CurrentFuelUnits > 0)
         {
-            this.rigidbody.AddForce(-this.transform.forward * (this.rigidbody.mass * ForwardPower));
+            this.GetComponent<Rigidbody>().AddForce(-this.transform.forward * (this.GetComponent<Rigidbody>().mass * ForwardPower));
             CurrentFuelUnits -= FUEL_CONSUMPTION_RATE;
         }
     }
@@ -49,7 +56,7 @@ public class VehicleController : MonoBehaviour {
     {
         if ((IsMoving || this.Movement == MovementType.Treads) && (CurrentFuelUnits > 0 || !ConsumeFuelOnTurn))
         {
-            this.rigidbody.AddTorque(Vector3.up * (this.rigidbody.angularDrag + TurningPower) * this.rigidbody.mass);
+            this.GetComponent<Rigidbody>().AddTorque(Vector3.up * (this.GetComponent<Rigidbody>().angularDrag + TurningPower) * this.GetComponent<Rigidbody>().mass);
             if (ConsumeFuelOnTurn)
             {
                 CurrentFuelUnits -= FUEL_CONSUMPTION_RATE;
@@ -61,7 +68,7 @@ public class VehicleController : MonoBehaviour {
     {
         if ((IsMoving || this.Movement == MovementType.Treads) && (CurrentFuelUnits > 0 || !ConsumeFuelOnTurn))
         {
-            this.rigidbody.AddTorque(Vector3.down * (this.rigidbody.angularDrag + TurningPower) * this.rigidbody.mass);
+            this.GetComponent<Rigidbody>().AddTorque(Vector3.down * (this.GetComponent<Rigidbody>().angularDrag + TurningPower) * this.GetComponent<Rigidbody>().mass);
             if (ConsumeFuelOnTurn)
             {
                 CurrentFuelUnits -= FUEL_CONSUMPTION_RATE;
@@ -71,7 +78,7 @@ public class VehicleController : MonoBehaviour {
 
     private bool CheckMoving()
     {
-        if (this.rigidbody.velocity.sqrMagnitude < MovingThreshold)
+        if (this.GetComponent<Rigidbody>().velocity.sqrMagnitude < MovingThreshold)
         {
             return false;
         }
